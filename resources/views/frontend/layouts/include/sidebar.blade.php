d4a62528d078b56fdbc87d3f2a7b37f76a3e2f64
shafiulsaurav <shafiulsaurav8@gmail.com>
2025-09-15 20:14:22 +0600

    a

--- resources/views/frontend/layouts/include/sidebar.blade.php
+++ resources/views/frontend/layouts/include/sidebar.blade.php
@@ -1,30 +1,23 @@
 <aside class="nav-sidebar">
     <div class="nav-header">
-        <a href=""><img loading="lazy" src="" alt="Online Shopping In Bangladesh With Home Delivery"></a>
-        <button class="nav-close"> <i class="fas fa-angle-left"></i></button>
+        @php
+            $logo_fav = \App\Models\LogoFavicon::first();
+        @endphp
+        <a href="{{ route('home') }}"><img loading="lazy" src="{{ asset($logo_fav->logo ?? 'uploads/logos/default.png') }}" alt="{{ $logo_fav->web_name ?? 'Online Shopping In Bangladesh With Home Delivery' }}"></a>
+        <button class="nav-close"> <i class="fas fa-times"></i></button>
     </div>
     <div class="nav-content">
         <ul class="nav-list">
+            @php
+                $categories = \App\Models\Category::where('is_active', 1)->get();
+            @endphp
+            @foreach($categories as $category)
             <li>
-                <a class="navbar-link " href="">
-                    Flash Sale
+                <a class="navbar-link" href="{{ route('category.products', $category->id) }}">
+                    {{ $category->name }}
                 </a>
             </li>
-            <li>
-                <a class="navbar-link " href="">
-                    Gadget &amp; Electronics
-                </a>
-            </li>
-            <li>
-                <a class="navbar-link " href="">
-                    Headphones
-                </a>
-            </li>
-            <li>
-                <a class="navbar-link " href="">
-                    Home and Living
-                </a>
-            </li>
+            @endforeach
         </ul>
     </div>
 </aside>
