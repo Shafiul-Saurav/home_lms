c76fc25d83f15dbd7682dbcee48d14504f4d5a3f
Shafiul-Ghani <shafiulgssaurav@gmail.com>
2026-06-07 15:36:35 +0600

    a

--- resources/views/frontend/pages/courses/course_video.blade.php
+++ resources/views/frontend/pages/courses/course_video.blade.php
@@ -6,11 +6,18 @@
     /* Video Meta Table Styling */
     .video-container {
         position: relative;
+        min-height: 420px;
+        background: #000;
+    }
+    .plyr__video-embed {
+        min-height: 420px;
+        position: relative;
+        z-index: 1;
     }
     .video-overlay {
         position: absolute;
         inset: 0;
-        z-index: 10;
+        z-index: 20;
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -249,8 +256,8 @@
                             <div class="plyr__video-embed" id="player" style="position: relative;">
                                 <!-- Custom thumbnail overlay -->
                                 <img id="thumbnail"
-                                    src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-                                    style="position: absolute; top: 0; left: 0; width: 100%; height: calc(10% + 30px); display: none; object-fit: cover; z-index: 10;">
+                                    src="{{ $course->image ? asset('uploads/courses/' . $course->image) : 'https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg' }}"
+                                    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; display: block; object-fit: cover; z-index: 5;">
 
                                 @if (!$isRestricted && $module->link)
                                     @if (Str::contains($module->link, ['youtube.com', 'youtu.be']))
