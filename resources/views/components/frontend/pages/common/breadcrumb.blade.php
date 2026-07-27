e96806a926a9431c3f576a9a1bb3c908eb0f57a7
Shafiul-Ghani <shafiulgssaurav@gmail.com>
2026-07-27 15:54:02 +0600

    a

--- resources/views/components/frontend/pages/common/breadcrumb.blade.php
+++ resources/views/components/frontend/pages/common/breadcrumb.blade.php
@@ -1,12 +1,13 @@
-@props(['title', 'breadcrumb', 'bgImage' => 'assets/frontend/img/breadcrumb/01.png'])
+@props(['title', 'breadcrumb', 'bgImage' => 'assets/frontend/img/breadcrumb/news-bg.png'])
 
-<div class="site-breadcrumb" style="position: relative; background: url({{ asset($bgImage) }}) no-repeat center center / cover;">
-    <div style="position: absolute; inset: 0; background: rgba(0, 0, 0, 0.55);"></div>
+<div class="site-breadcrumb"
+    style="position: relative; background: url({{ asset($bgImage) }}) no-repeat center center / cover;">
+    <div style="position: absolute; inset: 0; background: rgba(0, 0, 0, 0.308);"></div>
     <div class="container" style="position: relative; z-index: 1;">
         <h2 class="breadcrumb-title">{{ $title }}</h2>
         <ul class="breadcrumb-menu">
-            @foreach($breadcrumb as $item)
-                @if($loop->last)
+            @foreach ($breadcrumb as $item)
+                @if ($loop->last)
                     <li class="active">{{ $item['name'] }}</li>
                 @else
                     <li><a href="{{ $item['url'] }}">{{ $item['name'] }}</a></li>
